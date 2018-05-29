e6b3d4e02f8c91ac843c05e29f1f1098c8cc64b7
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-05-29 23:51:29 +0300

    29/05 23.50

--- js/common.js
+++ js/common.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
 
 	if (innerWidth>600) {
 		$('.rightSidebar__newssheet').jScrollPane();
+		$('.navMenu>ul>li:nth-child(6)>.submenu>.rightPart>.innerPart_wrap>.innerPart').jScrollPane();
+		$('.navMenu>ul>li:nth-child(7)>.submenu>.leftPart>.innerPart>ul').jScrollPane();
+		$('.navMenu>ul>li:nth-child(7)>.submenu>.rightPart>.innerPart>ul').jScrollPane();
+		$('.navMenu>ul>li:last-child>.submenu>.leftPart>.innerPart>ul').jScrollPane();
+		$('.navMenu>ul>li:last-child>.submenu>.rightPart>.innerPart>ul').jScrollPane();
 	}
 
 
@@ -19,6 +24,28 @@ $(document).ready(function(){
 
 	$('#toTop').click(function() {
 		$('body,html').animate({scrollTop:0},800);
+	});
+
+	var contentWidth = $(window).innerWidth();
+	$('.mainPart').css('width', contentWidth-440);
+
+
+	$( window ).resize(function() {
+	  var contentWidth = $(window).innerWidth();
+	  $('.mainPart').css('width', contentWidth-440);
+	});
+
+	$('.navMenu>ul>li:last-child>.submenu>.categoryPart>.innerPart>ul>li').click(function(){
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
+	});
+	$('.navMenu>ul>li:last-child>.submenu>.leftPart>.innerPart ul li').click(function(){
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
+	});
+	$('.navMenu>ul>li:nth-child(7)>.submenu>.leftPart>.innerPart ul li').click(function(){
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
 	});
 
 });
